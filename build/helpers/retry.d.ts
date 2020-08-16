9e7e91cb100a6364455ef5311ac189275d10c071
David Evans <davidje13@users.noreply.github.com>
2020-08-16 15:28:10 +0100

    Rebuild

--- build/helpers/retry.d.ts
+++ build/helpers/retry.d.ts
@@ -1,3 +1,9 @@
-declare const _default: (shouldRetry: (e: any) => boolean, maxAttempts?: number, baseDelayMillis?: number, attemptDelayMillis?: number, randomDelayMillis?: number) => <T>(fn: () => T | Promise<T>) => Promise<T>;
+declare const _default: (shouldRetry: (e: any) => boolean, { timeoutMillis, initialDelayMillis, maxDelayMillis, delayGrowth, jitter, }?: {
+    timeoutMillis?: number;
+    initialDelayMillis?: number;
+    maxDelayMillis?: number;
+    delayGrowth?: number;
+    jitter?: boolean;
+}) => <T>(fn: () => T | Promise<T>) => Promise<T>;
 export default _default;
 //# sourceMappingURL=retry.d.ts.map
